881811b31f069ec94bf88cd8ffbd68e27a148565
Duy Nguyễn <104379213+NguyenDuy38@users.noreply.github.com>
2023-11-07 17:54:44 +0700

    Full commit

--- routes/serviceRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const serviceCtl = require("../controllers/serviceController");
-
-const router = express.Router();
-router.get("/services", serviceCtl.getAllServices);
-router.get("/service/:id", serviceCtl.getServiceById);
-
-module.exports = router;
